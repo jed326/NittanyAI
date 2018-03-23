6604f5926b1a6ab8904c46bff94e69955d19f4c6
HopFr0g <connordmccandless@gmail.com>
2018-03-23 12:00:32 -0400

    Update fbookScraperAccess.py
    Now in python 3.6

--- UserInfoGeneration/fbookScraperAccess.py
+++ UserInfoGeneration/fbookScraperAccess.py
@@ -1,39 +1,45 @@
 # import some Python dependencies
 
-import urllib2
+import urllib.request
 import json
 import datetime
 import csv
 import time
 
-access_token = "access token here"
+access_token = ""
 
-def testFacebookPageData(access_token):
+def testFacebookPageData(access_token, request):
 
     has_next_page = True;
     
     # construct the URL string
     base = "https://graph.facebook.com/v2.12"
-    node = "/me/posts"
+    node = "/me/" + request
     parameters = "/?access_token=%s" % access_token
     url = base + node + parameters
+
+    #print(url)
 
     while has_next_page:
         
     
+        #print(url)
         # retrieve data
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
+        req = urllib.request.Request(url)
+        response = urllib.request.urlopen(req)
         data = json.loads(response.read())
         
         info = json.dumps(data, indent=4, sort_keys=True)
         print(info);
-
-
-        if 'paging' in data.keys():
+        
+        if 'paging' in data.keys() and 'next' in data['paging']:
                 url = data['paging']['next']
         else:
             has_next_page = False
 
-testFacebookPageData(access_token)
+
+#posts, likes, feed
+testFacebookPageData(access_token, "likes")
+
+
 
